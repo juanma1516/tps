38694797cb6732f5374a32a48f230df764c679ac
Juan Manuel <jmbc2715@gmail.com>
2021-08-21 12:56:19 -0300

    agregando las modificaciones

---
+++ js/holaMundo.js
@@ -0,0 +1,21 @@
+//Esto es un comentario simple de una sola linea
+/*Este es para comentar 
+en multiples lineas
+ */
+
+//mostrar mensaje en consola
+console.log("Este es un comentario en consola");
+
+//mostrar un msjh en el html
+document.write("Hello word");
+
+alert ("un mensaje")
+
+let pto3 = (3+5);
+document.write (pto3)
+
+prompt ("Hola nombreUsuario" , "introducir nombre de usuario");
+
+var a = (3)
+var b = (5)  
+var c = parentInt (a+b)
