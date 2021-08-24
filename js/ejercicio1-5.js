86fbc825091d9ab540079dc0b1f57d28bdd12a79
Juan Manuel <jmbc2715@gmail.com>
2021-08-24 11:39:20 -0300

    ejercicio 5 resuelto

---
+++ js/ejercicio1-5.js
@@ -0,0 +1,9 @@
+alert ("un mensaje")
+document.write ("hola mundo");
+//ejercicio 5 parseInt para hacer numerico al dato string del numero ingresado. Transforma texto a numerico entero//
+//parseFloat para decimales//
+
+let numero1 = parseInt(prompt("Ingrese un primer numero"));
+console.log (numero1)
+let numero2 = 50;
+console.log (numero1+numero2)
