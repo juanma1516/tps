ee87afde6d6f305c7b035ff05f3365afc604af44
Juan Manuel <jmbc2715@gmail.com>
2021-08-24 10:28:46 -0300

    variables que cambian

--- js/holaMundo.js
+++ js/holaMundo.js
@@ -8,14 +8,21 @@ console.log("Este es un comentario en consola");
 
 //mostrar un msjh en el html
 document.write("Hello word");
+//muestra una ventana emergente// 
+alert ("un mensaje");
 
-alert ("un mensaje")
+//crear o declarar variables var , let o const //
 
 let pto3 = (3+5);
-document.write (pto3)
+pto3 = 3
+
+document.write ("<br>variable:" + pto3);
 
 prompt ("Hola nombreUsuario" , "introducir nombre de usuario");
 
 var a = (3)
 var b = (5)  
-var c = parentInt (a+b)
+//modificar el valor de una variable (var o let)//
+b= 3;
+var c = (a+b)
+document.write("<br> cambio de variable" , c); 
