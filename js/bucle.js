76ae798af68b1badf3308c664b389ac53eb206b6
Juan Manuel <jmbc2715@gmail.com>
2021-08-24 15:49:11 -0300

    trabajado for, while y do while

---
+++ js/bucle.js
@@ -0,0 +1,33 @@
+
+//estructura while//
+/*
+while (condicion){
+    codigo a ejecutar si se cumple la condición
+    cambiar condicion
+}
+*/
+let contador = 0;
+while(contador <= 100){
+    document.write ("Elemento " + contador + "<br>")
+    contador++
+}
+//do while hacer... mientras (se compla la condicion)//
+/*
+do{
+    codigo a ejecutar
+    cambio de condicion 
+} while (condicion) */
+let elemento = 101;
+
+do{
+    document.write ("Elemento do-while " + elemento + "<br>");
+    elemento++;
+} while(elemento<=100);
+//Estructura for
+/*
+    for (crear variable; condicion; incremento o decrecimiento)
+
+    */
+   for(let i=1; i<=100 ; i + "<br>"){
+       document.write ("holis")
+   }
