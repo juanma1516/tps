c6fb895edec483850d5815b68d921435a8ad86bc
Juan Manuel <jmbc2715@gmail.com>
2021-09-01 14:50:49 -0300

    arrays terminado

--- js/arrays1.js
+++ js/arrays1.js
@@ -2,9 +2,31 @@ let anime = "DBZ";
 
 
 //arreglo
-let series = ["Dragon Ball", "Naruto", "Evangelion", anime, 15, true];
+let series = ["Dragon Ball","Naruto", "Evangelion", anime, 15, true];
  // crear un arreglo vacio
  let temporadas = [];
 //Acceder a un elemento del arreglo
-document.write("Serie: " + serie[1]);
-console.log ("Serie: " + serie[1])
+document.write("Serie: " + series[1]);
+console.log ("Serie: " + series[1]);
+//agregar un elemento al arreglo
+series.push(prompt("ingrese una serie"));
+console.log(series.length);
+console.log(series);
+//mostrar los elementos de un arreglo
+for(let posicion=0 ;posicion < series.length; posicion++){
+    document.write("<br>Serie: " + series[posicion])
+}
+document.write ("<br>");
+series[4] = "The office";
+for(let posicion=0 ;posicion < series.length; posicion++){
+    document.write("<br>Serie: " + series[posicion])
+}
+//agregar un elemento particular
+//el segundo valro del splice dice "cuantos queres borrar"
+//el primero la posicion
+//tercero cuantos o cual queres agregar
+series.splice(0,0, "one piece" );
+document.write ("<br>");
+for(let posicion=0 ;posicion < series.length; posicion++){
+    document.write("<br>Serie: " + series[posicion])
+}
