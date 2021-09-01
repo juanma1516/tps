73ecc71b3d19632df24e5043ecf6d79e385cdce0
Juan Manuel <jmbc2715@gmail.com>
2021-09-01 14:14:20 -0300

    empezamos arrays

---
+++ js/arrays1.js
@@ -0,0 +1,10 @@
+let anime = "DBZ";
+
+
+//arreglo
+let series = ["Dragon Ball", "Naruto", "Evangelion", anime, 15, true];
+ // crear un arreglo vacio
+ let temporadas = [];
+//Acceder a un elemento del arreglo
+document.write("Serie: " + serie[1]);
+console.log ("Serie: " + serie[1])
