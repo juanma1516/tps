6cf2ec3b4e562e4461fb1e8d71f3bae7e9a2d801
Juan Manuel <jmbc2715@gmail.com>
2021-09-15 14:31:47 -0300

    añadido exarrays

--- js/exarrays.js
+++ js/exarrays.js
@@ -12,3 +12,33 @@ debe mostrar el arreglo generado, luego realizar las siguientes acciones:
 ● Añade en última posición la ciudad de París.
 ● Escribe por pantalla el elemento que ocupa la segunda posición.
 ● Sustituye el elemento que ocupa la segunda posición por la ciudad de 'Barcelona'.*/
+
+ let town = [];
+
+do{
+     let addTown = prompt('Ingresar ciudad: ');
+     town.push(addTown);
+     console.log(town);
+ } while(confirm('¿Ingresar mas ciudades?'));
+
+ let count = town.length;
+
+ document.write(`-------------------------------------------------------`);
+ document.write(`<br>--|  La longitud del arreglo es de ${count}<br>`);
+ document.write(`-------------------------------------------------------`);
+ document.write(`<br>--|  En la primera posicion esta: ${town[0]}`);
+ document.write(`<br>--|  En la tercera posicion esta: ${town[2]}`);
+ document.write(`<br>--|  En la ultima posicion esta: ${town[(count)-1]}<br>`);
+ document.write(`-------------------------------------------------------`);
+ town.splice((count), 0, 'París');
+ count = town.length;
+ for(let i=0; i<count; i++){
+     document.write(`<br>ciudad :${town[i]}`);
+ }
+ document.write(`<br>-------------------------------------------------------`);
+ document.write(`<br>--|  En la segunda posicion esta: ${town[1]}`);
+ document.write(`<br>-------------------------------------------------------`);
+ town.splice(1, 1, 'Barcelona');
+ for(let i=0; i<count; i++){
+     document.write(`<br>ciudad :${town[i]}`);
+ }
